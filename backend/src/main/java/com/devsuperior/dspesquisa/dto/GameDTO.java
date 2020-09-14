c497c2f53caa2408646c83751f0248f2a325936b
gaturco <turco.gabriel@live.com>
2020-09-14 15:35:43 -0300

    DTO, Service, resource

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/dto/GameDTO.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dspesquisa.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.enumeration.Platform;
+
+public class GameDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String title;
+	private Platform platform;
+	
+	public GameDTO() {
+	}
+	
+	public GameDTO(Game game) {
+		id = game.getId();
+		title = game.getTitle();
+		platform = game.getPlatform();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Platform getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
+	
+	
+}
