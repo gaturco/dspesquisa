7caacff79997fe9c43ba522ded148ca7495c7900
gaturco <turco.gabriel@live.com>
2020-09-14 15:58:49 -0300

    POST records

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dspesquisa.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.services.RecordService;
+
+@RestController
+@RequestMapping(value = "/records")
+public class RecordResource {
+	
+	@Autowired
+	private RecordService service;
+	
+	@PostMapping
+	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO insertDTO) {
+		
+		RecordDTO dto = service.insert(insertDTO);
+		
+		return ResponseEntity.ok().body(dto);
+	}
+}
---
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.Record;
+import com.devsuperior.dspesquisa.repositories.GameRepository;
+import com.devsuperior.dspesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+	
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Record record = new Record();
+		record.setName(dto.getName());
+		record.setAge(dto.getAge());
+		record.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		record.setGame(game);
+		
+		record = repository.save(record);
+		
+		return new RecordDTO(record);
+	}
+
+}
