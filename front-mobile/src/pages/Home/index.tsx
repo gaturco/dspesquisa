368db25150f69b40350dad9ef17de3074a709f08
gaturco <turco.gabriel@live.com>
2020-09-18 14:39:23 -0300

    rotas no react native

--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Text, StyleSheet, View, Image, Alert } from 'react-native';
+import { Text, StyleSheet, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { FontAwesome5 as Icon} from '@expo/vector-icons';
+import Header from '../../components/Header';
+import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
+    const navigation = useNavigation();
+
     const handleOnPress = () => {
-        Alert.alert('botão')
+        navigation.navigate('CreateRecord')
     }
 
     return (
         <>
+            <Header />
             <View style={styles.container}>
                 <Image 
                     style={styles.gamerImage} 
