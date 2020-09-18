029257c4a12ac18253b6dc6b90a310bd3f45498e
gaturco <turco.gabriel@live.com>
2020-09-18 16:12:26 -0300

    configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-turco.herokuapp.com';
 
 const Charts = () => {
 
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-turco.herokuapp.com';
 
 const Records = () => {
 
